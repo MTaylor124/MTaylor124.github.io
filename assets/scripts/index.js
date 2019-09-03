1b000c61d7cf9fb049d9c233eca1df18cbc5a7ae
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-09-03 16:41:40 -0400

    Added functional sticky navbar

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,26 @@
+// 'use strict'
 $(() => {
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function () {
+    myScrollFunction()
+  }
 
+// Get the navbar
+  // const navbar = $('#mattnavbar')
+
+  const navbar = document.getElementById('mattnavbar')
+  const subnavbar = document.getElementById('subnavbar')
+// Get the offset position of the navbar
+  const sticky = navbar.offsetTop
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myScrollFunction () {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add('sticky')
+      subnavbar.classList.remove('stickyfix')
+    } else {
+      navbar.classList.remove('sticky')
+      subnavbar.classList.add('stickyfix')
+    }
+  }
 })
